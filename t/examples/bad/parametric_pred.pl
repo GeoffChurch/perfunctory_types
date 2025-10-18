158d3cc63b0d60973b4de8e0e21536c4641cd55f
Geoffrey Churchill <geoffrey.a.churchill@gmail.com>
2025-10-18 12:21:51 -0400

    Describe parametric_pred.pl failure

--- t/examples/bad/parametric_pred.pl
+++ t/examples/bad/parametric_pred.pl
@@ -1,6 +1,6 @@
 :- use_module(library(perfunctory_types)).
 
-:- type _ ---> p(_).
+:- type _ ---> p(_). % p is parametric
 
-p(a).
-p(b).
+p(a). % ok, this clause's head has type p(a)
+p(b). % FAIL, p(b) is syntactically well-formed but the predicate fails the semantic check
